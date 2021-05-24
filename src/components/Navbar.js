0dcfbd3d26475c348a39f72286b9a1262a0e8747
kuldeep.panwar007 <kuldeeppanwar7697@gmail.com>
2021-05-24 18:18:37 +0530

    responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 // import {Route} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import Clock from "./Clock";
 
-import { NavLink } from 'react-router-dom'
+// function toggle(){
+//   console.log("ok");
+//   const togglec = document.getElementsByClassName("toggle-menu");
+//   toggle.classli
+// }
+
 function Navbar() {
+  const[isActive, setActive] = useState(false);
+  const[k, setK] = useState(false);
+
+
+  // let k = "x";
+  const toggleClass = () => {
+      //  k="y";
+    setK(!k)
+    setActive(!isActive);
+    return(k)
+  };
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        
-       
+
 
         <div className="collapse navbar-collapse" id="navbar-div">
-          <ul className="navbar-nav" >
-            <li className="nav-item active">
+          <ul  className={isActive ? 'activetoggle' : "navbar-nav"} >
+            <li className="nav-item active" >
               <NavLink className="nav-link" to="/my-app">Home</NavLink>
             </li>
             <li className="nav-item">
@@ -36,10 +52,12 @@ function Navbar() {
             <li className="nav-item">
               <NavLink className="nav-link" to="/my-app/Clock">Clock</NavLink>
             </li>
-            <li className="nav-item"> <Clock/></li>
+            <li className="nav-item"> <Clock /></li>
             {/* <li className="nav-item"> </li> */}
 
           </ul>
+          <div id="toggle-menu" className={isActive ? 'activetoggle' : ""}
+            onClick={toggleClass}>{k? <i class="fas fa-times"></i> :<i class="fas fa-bars"></i>}</div>
 
         </div>
       </nav>
